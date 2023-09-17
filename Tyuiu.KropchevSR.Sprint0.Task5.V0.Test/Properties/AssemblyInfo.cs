fff1952e36a3d987f178ca7b2cad7fbe50add7a6
Tyuiu.KropchevSR@outlook.com <Tyuiu.KropchevSR@outlook.com>
2023-09-17 18:35:31 +0500

    Task5

---
+++ Tyuiu.KropchevSR.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KropchevSR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KropchevSR.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02dabc53-bd02-4ba5-a273-ba8c26386e47")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KropchevSR.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KropchevSR.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KropchevSR.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(10, 5));
+            Console.WriteLine("A - B = " + DataService.Substration(10, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 5));
+            Console.WriteLine("A / B = " + DataService.Division(10, 0));
+            Console.ReadKey();
+        }
+    }
+}
